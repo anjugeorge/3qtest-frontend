048732f67c5689459900c4fe1ad3339c36872279
Anju George <anjugeorgeantony@gmail.com>
2025-12-03 17:51:06 -0500

    Changed pdf design

--- src/components/pages/Assessment/Result.jsx
+++ src/components/pages/Assessment/Result.jsx
@@ -436,19 +436,20 @@ const Result = () => {
     const element = document.getElementById("forPDF");
 
     const header = document.createElement("div");
-    header.innerHTML = ` <div style="display:flex; margin:10px;">
+    header.innerHTML = ` <div style="display:flex; margin:15px; ">
               <img src="/assets/favicon.ico" style="width:32px; height:32px" />
               <h1 style="font-size:20px; font-weight:bold; font-style:italic; color:#3B0764;">
                 3QTest
               </h1>
               </div>
-              <div style="margin-bottom:10px;">
-              <h1 style="font-size:16px; font-weight:bold; font-style:italic; color:#hhhhhh;">
+              <div style="margin:15px;">
+              <h1 style="font-size:16px; font-weight:bold; font-style:italic;">
                 Career Assessment Report
               </h1>
               <div/>
               `;
     const clone = element.cloneNode(true);
+    clone.style.backgroundColor = "white";
     const canvas = element.querySelector("canvas");
     const clonedCanvas = clone.querySelector("canvas");
     if (canvas) {
@@ -464,7 +465,7 @@ const Result = () => {
       el.style.fontSize = "16px";
     });
     const options = {
-      margin: [5, 5, 5, 5],
+      margin: [15, 5, 15, 5],
       filename: "Career Assessment Report.pdf",
 
       image: { type: "jpeg", quality: 0.8 },
@@ -472,6 +473,12 @@ const Result = () => {
         scale: 3,
       },
       jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+      pagebreak: {
+        mode: ["avoid-all", "css", "legacy"],
+        before: ".page-break-before",
+        after: ".page-break-after",
+        avoid: ".page-break-avoid",
+      },
     };
 
     html2pdf()
@@ -546,10 +553,10 @@ const Result = () => {
                       <button
                         onClick={generatePdfAndSendEmail}
                         type="button"
-                        className="bg-purple-900 hover:bg-purple-950  w-48 h-10 text-white text-[14px] font-bold rounded-lg flex justify-center items-center gap-1"
+                        className="mb-1 bg-purple-600 hover:bg-purple-700  w-32 h-10 text-white text-[14px] font-bold rounded-lg flex justify-center items-center gap-1"
                       >
                         <IoIosSend className="text-lg" />
-                        Send Results to Email
+                        Email Report
                       </button>
                       {isLoading && (
                         <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm">
@@ -559,19 +566,20 @@ const Result = () => {
                             disabled
                           >
                             <svg
-                              className="mr-3 h-5 w-5 animate-spin text-white"
-                              viewBox="0 0 24 24"
-                              xmlns="http://www.w3.org/2000/svg"
+                              aria-hidden="true"
+                              className="w-4 h-4 text-neutral-tertiary animate-spin fill-brand me-2"
+                              viewBox="0 0 100 101"
                               fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
                             >
-                              <circle
-                                className="opacity-30"
-                                cx="15"
-                                cy="15"
-                                r="10"
-                                stroke="currentColor"
-                                strokeWidth="4"
-                              ></circle>
+                              <path
+                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                fill="currentColor"
+                              />
+                              <path
+                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                fill="currentFill"
+                              />
                             </svg>
                             Sending...
                           </button>
@@ -819,7 +827,7 @@ const Result = () => {
                               What's your power
                             </h1>
                             <div
-                              className="py-5 grid md:grid-cols-3 grid-cols-1 gap-4 "
+                              className="p-5 grid md:grid-cols-3 grid-cols-1 gap-4 "
                               data-aos="fade-up"
                               data-aos-duration="1500"
                             >
@@ -891,7 +899,7 @@ const Result = () => {
                             Data Visualization{" "}
                           </h1>
                           <div
-                            className="py-5 grid md:grid-cols-2 grid-cols-1 gap-4"
+                            className="p-5 grid md:grid-cols-2 grid-cols-1 gap-4"
                             data-aos="fade-up"
                             data-aos-duration="1500"
                           >
@@ -929,7 +937,7 @@ const Result = () => {
                               Weakness to Transform
                             </h1>
                             <div
-                              className="py-5 grid md:grid-cols-3 grid-cols-1 gap-4"
+                              className="p-5 grid md:grid-cols-3 grid-cols-1 gap-4"
                               data-aos="fade-up"
                               data-aos-duration="1500"
                             >
@@ -1070,37 +1078,43 @@ const Result = () => {
                             )}
                           </div>
                         </div>
-
-                        {bestCareer.primaryCareerMatch
-                          ?.seekingNewChallenges && (
-                          <div className="p-5 bg-[#e920630d]  rounded-md mb-10">
-                            <p className="text-lg md:text-xl leading-[2] px-30 font-roboto font-bold py-2 text-[#b5053fdb]">
-                              <FaMountain
-                                size={60}
-                                style={{
-                                  backgroundColor: "#e920630d",
-                                  border: "solid 5px #b5053fdb",
-                                  borderRadius: "10px",
-                                  padding: "5px",
-                                  margin: "5px",
-                                }}
-                              />
-                              {
-                                bestCareer.primaryCareerMatch
-                                  ?.seekingNewChallenges?.title
-                              }
-                            </p>
-                            <p
-                              className="text-md/7 font-roboto leading-7 text-justify"
-                              style={{ whiteSpace: "pre-line" }}
-                            >
-                              {
-                                bestCareer.primaryCareerMatch
-                                  ?.seekingNewChallenges?.desc
-                              }
-                            </p>
-                          </div>
-                        )}
+                        <div
+                          style={{
+                            pageBreakBefore: "always",
+                          }}
+                        >
+                          {" "}
+                          {bestCareer.primaryCareerMatch
+                            ?.seekingNewChallenges && (
+                            <div className="5 p-5 bg-[#e920630d]  rounded-md mb-10">
+                              <p className="text-lg md:text-xl leading-[2] px-30 font-roboto font-bold py-2 text-[#b5053fdb]">
+                                <FaMountain
+                                  size={60}
+                                  style={{
+                                    backgroundColor: "#e920630d",
+                                    border: "solid 5px #b5053fdb",
+                                    borderRadius: "10px",
+                                    padding: "5px",
+                                    margin: "5px",
+                                  }}
+                                />
+                                {
+                                  bestCareer.primaryCareerMatch
+                                    ?.seekingNewChallenges?.title
+                                }
+                              </p>
+                              <p
+                                className="text-md/7 font-roboto leading-7 text-justify"
+                                style={{ whiteSpace: "pre-line" }}
+                              >
+                                {
+                                  bestCareer.primaryCareerMatch
+                                    ?.seekingNewChallenges?.desc
+                                }
+                              </p>
+                            </div>
+                          )}
+                        </div>
 
                         {bestCareer.primaryCareerMatch?.whatToImprove && (
                           <div className="p-5 bg-[#E0F2FE]  rounded-md mb-10">
